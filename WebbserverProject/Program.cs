28f716e2c91a342057e18279d32a1f849b36ab2d
Gerglamesh <hampe.kjellstrand@gmail.com>
2020-08-31 14:01:53 +0200

    Add project

---
+++ WebbserverProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebbserverProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
